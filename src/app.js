fb9291686241c3f28d5db78a6b6a7765157581de
ankitsinghhh <ankitsingh79834@gmail.com>
2024-10-09 12:50:08 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,41 @@ const app = express()
 // + -- you can write as many of previous letter
 // /a/ --this regex means that if that routee contains letter 'a' it will work 
 
+// app.use -> it handles any type of request method that is -> post or get or delete or put or patch
+// app.get -> it handles only GET requests
+//app.post -> it handles only POST requests
+//app.delete -> it handles only DELETE requests
+
+app.use("/login",
+    (req,res,next) => {
+    console.log("handling login route, 1st Response!!!!!!!!!!!!")
+
+    next()
+    // res.send("This is login route handler")
+    },
+    (req,res,next) => {
+        console.log("handling login route, 2nd Response!!!!!!!!!!!!")
+
+        // res.send("2nd Response!!!!!!")
+        next()
+    }
+    ,
+    (req,res,next) => {
+        console.log("handling login route, 3rd Response!!!!!!!!!!!!")
+
+        // res.send("3rd Response!!!!!!")
+        next()
+    }
+    ,
+    (req,res,next) => {
+        console.log("handling login route, 4th Response!!!!!!!!!!!!")
+        res.send("4th Response!!!!!!")
+        // next()
+
+    }
+)
+
+// we can also wrap all callback functions for route handling in an array , or some of them -> everything will be same , no functionality will change 
 
 //handling get request
 app.get('/user/:userId/:name/:pwd',(req,res) =>{
