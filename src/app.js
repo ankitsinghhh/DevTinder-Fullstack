e459814b2b8a484436ad8e1ae03b127cf6200d29
ankitsinghhh <ankitsingh79834@gmail.com>
2024-10-14 17:32:05 +0530

    database initialization

--- src/app.js
+++ src/app.js
@@ -1,10 +1,67 @@
+const express = require('express');
+const { connectDB } = require('./config/database');
+const User = require('./models/user');  // Import the model correctly
+
+const app = express();
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    console.log("signup route called");
+
+    const userObj = {
+        "firstName": "daemon",
+        "lastName": "targaryen",
+        "email": "daemon@gmail.com",
+        "password": "12345678",
+        "age": 35,
+        "gender": "Male",
+      
+    }
+
+    const user = new User(userObj);
+
+   try{
+    await user.save();
+    res.send("user added successfully")
+   }
+   catch(err){
+    res.status(400).send("error saving the user" + err.message)
+   }
+   
+});
 
 
-//creating a server using express js 
 
-const express = require('express')
+connectDB()
+    .then(() => {
+        console.log("Database connection established");
+        app.listen(3000, () => {
+            console.log("Server is successfully listening on port 3000");
+        });
+    })
+    .catch((err) => {
+        console.error("Database connection failed:", err);
+    });
 
-const app = express()
+app.get("/", (req, res) => {
+    res.send("Handling root route");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ! NOTES --- > 
 
 // ? -- optional 
 // * -- you can write anything in between the previous and next one 
@@ -87,70 +144,67 @@ const app = express()
 //     }
 // })
 
-const {adminAuth,userAuth} = require("./middlewares/auth") // middlwares imported from middlware file
-app.use("/admin",adminAuth)
-// app.use("/user",userAuth) // to check auth in all routes of /user/<routename>
+// const {adminAuth,userAuth} = require("./middlewares/auth") // middlwares imported from middlware file
+// app.use("/admin",adminAuth)
+// // app.use("/user",userAuth) // to check auth in all routes of /user/<routename>
 
-app.get(
-    "/admin/getAllData",
-    (req,res,next) =>{
+// app.get(
+//     "/admin/getAllData",
+//     (req,res,next) =>{
         
-        //LOGIC OF FETCHING ALL DATA
+//         //LOGIC OF FETCHING ALL DATA
         
-        res.send("all data sent")
-    }
-)
+//         res.send("all data sent")
+//     }
+// )
 
-app.get(
-    "/admin/deleteUser/:id",
-    (req,res,next) =>{
-        //LOGIC OF FETCHING ALL DATA
+// app.get(
+//     "/admin/deleteUser/:id",
+//     (req,res,next) =>{
+//         //LOGIC OF FETCHING ALL DATA
         
-        res.send(`User with id ${req.params.id} deleted`)
+//         res.send(`User with id ${req.params.id} deleted`)
         
      
-    }
-)
+//     }
+// )
 
-app.get("/user",(req,res,next)=>{
+// app.get("/user",(req,res,next)=>{
 
-    console.log(req.query)
-    res.send("user route is called")
-})
+//     console.log(req.query)
+//     res.send("user route is called")
+// })
 
-app.get("/user/login", (req,res,next) =>{
+// app.get("/user/login", (req,res,next) =>{
 
-    res.send("User logged in successfully")
-})
+//     res.send("User logged in successfully")
+// })
 
-app.get("/user/data", userAuth,(req,res,next) =>{ // to check in this route
+// app.get("/user/data", userAuth,(req,res,next) =>{ // to check in this route
 
-    res.send("User data fetched successfully")
-})
+//     res.send("User data fetched successfully")
+// })
 
-app.get("/getUserData", (req,res)=>{
+// app.get("/getUserData", (req,res)=>{
 
-   try{
-    //logic of fetching user data from database
-    throw new Error("rnadomdsljfl;dsfj;ladfj;sdfj")
-    res.send("user data sent successfully!!!!!!!!!")
-   }
-   catch(err){
-    res.status(500).send("something went wrong while fetching data")
-   }
-})
+//    try{
+//     //logic of fetching user data from database
+//     throw new Error("rnadomdsljfl;dsfj;ladfj;sdfj")
+//     res.send("user data sent successfully!!!!!!!!!")
+//    }
+//    catch(err){
+//     res.status(500).send("something went wrong while fetching data")
+//    }
+// })
 
-app.use("/", (err,req,res,next)=>{
-    //error handling for all routes where error is unhandled by them internally
-    if(err){
-        res.status(500).send("something went Wrong")
-    }
+// app.use("/", (err,req,res,next)=>{
+//     //error handling for all routes where error is unhandled by them internally
+//     if(err){
+//         res.status(500).send("something went Wrong")
+//     }
 
-})
+// })
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000')
-})
 
 // app.use("/",(req,res)=>{
 //     res.send("welcome to my first server , port used: 3000")
