200cf971c19a853df81155088f6d010a6e58070d
ankitsinghhh <ankitsingh79834@gmail.com>
2025-08-07 22:28:48 +0530

    updating the code to host frontend on vercel and backend on render

--- DevTinder-Backend/src/app.js
+++ DevTinder-Backend/src/app.js
@@ -9,15 +9,17 @@ const http = require('http')
 require("./utils/cronjob.js")
 
 
+
 // Creating an instance/object of an Express application
 const app = express();
 //middlware to parse the json data coming from the request body to the javascript object
 app.use(
   cors({
-        origin: "http://localhost:5173", // this is the frontend url
+        origin: ["http://localhost:5173","https://devtinder-fullstack-backend.onrender.com"], // this is the frontend url
         credentials: true, // this is to allow the cookies to be sent to the frontend
       })
 )
+// https://devtinder-fullstack-backend.onrender.com
 app.use(express.json())
 app.use(cookieParser())
 
--- DevTinder-Frontend/src/utils/constants.js
+++ DevTinder-Frontend/src/utils/constants.js
@@ -4,5 +4,7 @@
 //production
 // export const BASE_URL =  "/api"
 
-//making it dynamic so it happens automatically 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api"
+//making it dynamic so it happens automatically - To host on AWS 
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api"
+// to host the frontend on vercel and backend on render 
+export const BASE_URL = location.hostname === "localhost" ? "https://devtinder-fullstack-backend.onrender.com"
